5ef8da826f2839667775d654e568a59c36aa9f87
Diana Andrade <dianapaolaandradeguevara@gmail.com>
2025-12-04 14:08:56 -0600

    subiendo

---
+++ models/sensor.model.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const sensorSchema = new Schema({
+  type: {
+    type: String,
+    required: true,
+    enum: ['temperature', 'humidity', 'co2', 'noise'],
+    trim: true
+  },
+
+  unit: {
+    type: String,
+    required: true,
+    enum: ['°C', '%', 'ppm'],
+  },
+
+  model: {
+    type: String,
+    required: true,
+    trim: true
+  },
+
+  location: {
+    type: String,
+    required: true
+  },
+
+  isActive: {
+    type: Boolean,
+    default: true
+  },
+
+  deviceId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Device',
+    required: true
+  },
+
+  installedAt: {
+    type: Date,
+    default: Date.now
+  }
+},
+{
+  timestamps: true
+});
+
+module.exports = mongoose.model('Sensor', sensorSchema);
